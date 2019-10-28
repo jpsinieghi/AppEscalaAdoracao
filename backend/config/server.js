74634bc4569a7d025973eaa4806d058a51cb4f7d
Joao Sinieghi <jpsinieghi@gmail.com>
2019-10-28 16:48:50 -0300

    first commit3

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,15 @@
 const port = 3003
-const bodyParser = required('body-parser')
+const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 
+server.listen(port, function(){
+    console.log(`Backend rodando na porta ${port}`)
+})
 
+server.use(function(req, res, next) {
+    res.send('Funcionou')
+})
