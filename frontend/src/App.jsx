023ab09d929d0db7a86b7f03fedae58777d9fcd6
Joao Sinieghi <jpsinieghi@gmail.com>
2021-04-30 16:40:28 -0300

    acerto fatoracao

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,10 @@ import Details from './Details/Details'
 import Calendar from './Calendar/Calendar'
 import Members from './Members/Members'
 import 'semantic-ui-css/semantic.min.css'
-// import { Segment, Header, Grid, GridColumn } from "semantic-ui-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap'
-
-
 import axios from 'axios';
+
 
 //Import immutability-helper
 import update from 'immutability-helper';
@@ -26,110 +24,110 @@ import update from 'immutability-helper';
 export const AppContext = React.createContext();
 
 
-export default function App () {
+export default function App() {
 
 
   // Set up Initial State
-const initialState = {
-  daySelected: [],
-  hoursofdaySelected: [],
-  // hourSelected: [],
-  showDetails: false,
-  showmemberDetails: false,
-  showHours: false,
-  allMembers: [],
-  memberDetails: [],
-  hourselected: null,
-  logEventos: []
+  const initialState = {
+    daySelected: [],
+    hoursofdaySelected: [],
+    // hourSelected: [],
+    showDetails: false,
+    showmemberDetails: false,
+    showHours: false,
+    allMembers: [],
+    memberDetails: [],
+    hourselected: null,
+    logEventos: []
 
- 
-};
 
-function reducer(state, action) {
-  switch (action.type) {
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
       case 'DAYSELECTED':
-          return update(state, { daySelected: {$set: action.data}});
+        return update(state, { daySelected: { $set: action.data } });
 
       case 'HOURSOFDAYSELECTED':
-          return update(state, { hoursofdaySelected: {$set: action.data}});
+        return update(state, { hoursofdaySelected: { $set: action.data } });
 
       // case 'HOURSELECTED':
       //     return update(state, { hourSelected: {$set: action.data}});
-  
+
       case 'SHOWDETAILS':
-          return update(state, { showDetails: {$set: action.data}});
+        return update(state, { showDetails: { $set: action.data } });
 
       case 'SHOWMEMBERDETAILS':
-          return update(state, { showmemberDetails: {$set: action.data}});
+        return update(state, { showmemberDetails: { $set: action.data } });
 
-       case 'SHOWHOURS':
-             return update(state, { showHours: {$set: action.data}});
-            
+      case 'SHOWHOURS':
+        return update(state, { showHours: { $set: action.data } });
+
       case 'HOURSELECTED':
-            return update(state, { hourselected: {$set: action.data}});
-      
+        return update(state, { hourselected: { $set: action.data } });
+
       case 'MEMBERDETAILS':
-            return update(state, { memberDetails: {$set: action.data}});
-      
+        return update(state, { memberDetails: { $set: action.data } });
+
       case 'MEMBERDATA':
-            return update(state, { allMembers: {$set: action.data}});
-      
+        return update(state, { allMembers: { $set: action.data } });
 
-                  
       default:
-          return initialState;
+        return initialState;
+    }
   }
-}
-
-const fetchData = async () => {
-    const result = await axios(`http://localhost:3001/api/members/`,)
-    initialState.allMembers = result.data
-    
-  }
-
-useEffect(() => {fetchData()},[])
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-   return(<div>
-      <AppContext.Provider value={{ state, dispatch }}>
+  //Pega todos os Membros por API no Backend para alimentar a variavel initialState.allMembers
+  const fetchData = async () => {
+    const result = await axios(`http://localhost:3001/api/members/`,)
+    initialState.allMembers = result.data
+  }
+  useEffect(() => { fetchData() }, []) 
+
+  
+
+  return (<div>
+    <AppContext.Provider value={{ state, dispatch }}>
       <Container>
-          <Row><Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="">Home</Link>
-            </li>
-            <li>
-            <Link to="/membro">Membro</Link>
-          </li>
-        </ul>
-        <hr />
-       
-        <Switch>
-        
-          <Route exact path="/">
-            <Row>
-            <Col md="auto"><Calendar/></Col>
-            <Col md="auto"><Day /></Col>
-            <Col>{state.showDetails && <Details/>}</Col>
-           
-            </Row>
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/membro">
-            <Row>
-              <Members />
-            </Row>
-          </Route>
-          
-        </Switch>
-        
-      </div>
-    </Router>
-    </Row>
-    </Container>
+        <Row>
+          <Router>
+            <div>
+              <ul>
+                <li>
+                  <Link to="">Home</Link>
+                </li>
+                <li>
+                  <Link to="/membro">Membro</Link>
+                </li>
+              </ul>
+              <hr />
+
+              <Switch>
+
+                <Route exact path="/">
+                  <Row>
+                    <Col md="auto"><Calendar /></Col>
+                    <Col md="auto"><Day /></Col>
+                    <Col>{state.showDetails && <Details />}</Col>
+
+                  </Row>
+                </Route>
+              </Switch>
+              <Switch>
+                <Route exact path="/membro">
+                  <Row>
+                    <Members />
+                  </Row>
+                </Route>
+
+              </Switch>
+
+            </div>
+          </Router>
+        </Row>
+      </Container>
     </AppContext.Provider>
-    </div>)
+  </div>)
 }
--- frontend/src/Calendar/Calendar.jsx
+++ frontend/src/Calendar/Calendar.jsx
@@ -3,34 +3,34 @@ import { AppContext } from "../App"
 import DatePicker, { registerLocale } from "react-datepicker";
 import ptBR from "date-fns/locale/pt-BR"
 import "react-datepicker/dist/react-datepicker.css";
-registerLocale("pt-BR", ptBR); 
+registerLocale("pt-BR", ptBR);
 
 
 
 export default function Calendar() {
-    
-    const {dispatch} = useContext(AppContext);
+
+    const { dispatch } = useContext(AppContext);
 
     const changeInputValue = (newValue) => {
 
-        dispatch({ type: 'DAYSELECTED', data: newValue.valueOf(),});
+        dispatch({ type: 'DAYSELECTED', data: newValue.valueOf(), });
         // dispatch({ type: 'HOURSOFDAYSELECTED', data: [],});
-        dispatch({ type: 'SHOWHOURS', data: true,});
-        dispatch({ type: 'SHOWDETAILS', data: false,});
+        dispatch({ type: 'SHOWHOURS', data: true, });
+        dispatch({ type: 'SHOWDETAILS', data: false, });
     };
 
 
-                    return(
-                            <div className="Calendar">
-                            <DatePicker
-                                onChange={changeInputValue}
-                                locale="pt-BR"
-                                inline
-                                />
-                            
-               
-                            </div>
-                    )
+    return (
+        <div className="Calendar">
+            <DatePicker
+                onChange={changeInputValue}
+                locale="pt-BR"
+                inline
+            />
+
+
+        </div>
+    )
 
 
 
