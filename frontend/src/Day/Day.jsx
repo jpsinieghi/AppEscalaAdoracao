df9abdce06a9857d99c96440c204030ec6bee7d6
Joao Sinieghi <jpsinieghi@gmail.com>
2021-05-05 15:47:38 -0300

    aumento de horas

--- frontend/src/Day/Day.jsx
+++ frontend/src/Day/Day.jsx
@@ -3,9 +3,10 @@ import { AppContext } from "../App"
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap'
+import "./Day.css"
 
 
-const HORAS = [8, 9, 10, 11, 12, 13]
+const HORAS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
 
 export default function Day() {
